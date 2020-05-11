c0f039cde776f39e4f727785f8ec77c7c08ead65
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-11 12:20:42 +0500

    add 3rd example

--- instance.method.py
+++ instance.method.py
@@ -7,9 +7,13 @@ class Person:
     def name_class(self):
         return f"{self.Name} {self.Class}"
 
+    def over_18(self):
+        return self.Age > 18
+
 
 p1 = Person("izhar", 12, 24)
-p2 = Person("shery", 14, 26)
+p2 = Person("shery", 14, 10)
 
 print(p1.Name)
 print(p2.name_class())
+print(p2.over_18())
