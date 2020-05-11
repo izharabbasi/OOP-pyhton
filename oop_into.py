8f57414b44476236b9d07a200aff2207b7f21f48
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-11 12:05:01 +0500

    add 1st example

--- oop_into.py
+++ oop_into.py
@@ -1,1 +1,12 @@
-print("hello wolrd")
+class Person:
+    def __init__(self, Name, Class, Age):
+        self.Name = Name
+        self.Class = Class
+        self.Age = Age
+
+
+p1 = Person("izhar", 12, 24)
+p2 = Person("shery", 10, 26)
+
+print(p1.Name)
+print(p2.Age)
