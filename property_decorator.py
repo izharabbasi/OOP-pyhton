9b90e8a8bfc28d716e51b655ca0cd76ada86618a
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-22 00:22:04 +0500

    add 1st example

--- property_decorator.py
+++ property_decorator.py
@@ -1,1 +1,17 @@
-print("hello world")
+class Phone:
+    def __init__(self, make, model, price):
+        self.make = make
+        self.model = model
+        self.price = price
+        self.complete_specs = f"{self.make} {self.model} and price is {self.price}"
+
+    def calling(self, Phone_number):
+        print(f"Calling {Phone_number}")
+
+    def full_name(self):
+        return f"{self.make} and  {self.model}"
+
+
+phone1 = Phone("Iphone", "7Plus", 6500)
+
+print(phone1.model)
