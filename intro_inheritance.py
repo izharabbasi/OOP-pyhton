9b7770ceb98a40fe88a5e80e4b4c0ea5006f1d45
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-22 00:45:40 +0500

    add 1st example

--- intro_inheritance.py
+++ intro_inheritance.py
@@ -1,1 +1,11 @@
-print("hello world")
+class Phone:
+    def __init__(self, make, model, _price):
+        self.make = make
+        self.model = model
+        self._price = _price
+
+    def make_call(self, number):
+        print(f"Calling {number}")
+
+    def full_name(self):
+        return f"{self.make} {self.model} and price is {self._price}"
